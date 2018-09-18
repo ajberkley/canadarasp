0421d8121b3f16be48a05d8661f83d475b4146d8
Andrew Berkley <ajberkley@gmail.com>
2018-09-18 22:28:34 +0000

    More bugfixes

--- aws-utils/download-data-and-start-server.sh
+++ aws-utils/download-data-and-start-server.sh
@@ -2,10 +2,10 @@
 MODEL=${1:-"gdps"}
 cd /home/ubuntu/canadarasp/aws-utils
 source create-download-box.sh /mnt
-cd ../continental-test
+cd /home/ubuntu/canadarasp/continental-test
 source guess-time.sh $MODEL
-source download-data.sh $MODEL
-cd ../aws-utils
-source unmount-download-box.sh
-source start-compute-server.sh
+download-data.sh $MODEL
+cd /home/ubuntu/canadarasp/aws-utils
+unmount-download-box.sh
+start-compute-server.sh
 ( sleep 7200 ; source stop-compute-server.sh )
