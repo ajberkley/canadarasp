7fe3011133a0bf07d698023e659bdfa5f30e2fa0
Andrew Berkley <ajberkley@gmail.com>
2018-09-21 02:32:56 +0000

    Update to named download-boxes

--- aws-utils/create-download-box.sh
+++ aws-utils/create-download-box.sh
@@ -3,12 +3,13 @@
 # First check to make sure a download-box does not exist already
 MNT=${1:-/mnt}
 export -n VOL_ID=
-export VOL_ID=`aws ec2 describe-volumes --filters Name=tag:Name,Values=download-box Name=status,Values=available | head -1 | awk '{ print $7 }'`
+export DBOXNAME=download-box-$MODEL
+export VOL_ID=`aws ec2 describe-volumes --filters Name=tag:Name,Values=$DBOXNAME Name=status,Values=available | head -1 | awk '{ print $7 }'`
 export MY_INSTANCE_ID=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
 export MY_ZONE=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
 if [ -z $VOL_ID ]; then
-  echo No download-box available, creating one
-  export VOL_ID=`aws ec2 create-volume --no-encrypted --availability-zone $MY_ZONE --size 30 --volume-type standard --tag-specifications 'ResourceType=volume,Tags=[{Key=Name,Value=download-box}]' | head -1 | awk '{ print $6 }'`
+  echo No $DBOXNAME available, creating one
+  export VOL_ID=`aws ec2 create-volume --no-encrypted --availability-zone $MY_ZONE --size 30 --volume-type standard --tag-specifications 'ResourceType=volume,Tags=[{Key=Name,Value=$DBOXNAME}]' | head -1 | awk '{ print $6 }'`
   echo Created $VOL_ID in $MY_ZONE
 else
   echo Volume $VOL_ID already available, using it
--- aws-utils/download-data-and-start-server.sh
+++ aws-utils/download-data-and-start-server.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 MODEL=${1:-"gdps"}
 echo Downloading data for $MODEL
+DBOXNAME=download-box-$MODEL
 cd /home/ubuntu/canadarasp/aws-utils
-source create-download-box.sh /download-box
+source create-download-box.sh /$DBOXNAME
 source aws-write-tag.sh `./get-compute-server-id.sh` model $MODEL
 cd /home/ubuntu/canadarasp/continental-test
 source guess-time.sh $MODEL
-./download-data.sh $MODEL /download-box
+./download-data.sh $MODEL /$DBOXNAME
 cd /home/ubuntu/canadarasp/aws-utils
 source unmount-download-box.sh
 source start-compute-server.sh
