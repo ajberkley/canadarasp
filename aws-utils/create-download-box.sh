0a71b978a97c5841919bc1b19be437a757d5ce9d
Andrew Berkley <ajberkley@gmail.com>
2018-09-21 02:45:29 +0000

    Bugfix to naming

--- aws-utils/create-download-box.sh
+++ aws-utils/create-download-box.sh
@@ -3,13 +3,13 @@
 # First check to make sure a download-box does not exist already
 MNT=${1:-/mnt}
 export -n VOL_ID=
-export DBOXNAME=download-box-$MODEL
+export DBOXNAME="download-box-$MODEL"
 export VOL_ID=`aws ec2 describe-volumes --filters Name=tag:Name,Values=$DBOXNAME Name=status,Values=available | head -1 | awk '{ print $7 }'`
 export MY_INSTANCE_ID=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
 export MY_ZONE=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
 if [ -z $VOL_ID ]; then
   echo No $DBOXNAME available, creating one
-  export VOL_ID=`aws ec2 create-volume --no-encrypted --availability-zone $MY_ZONE --size 30 --volume-type standard --tag-specifications 'ResourceType=volume,Tags=[{Key=Name,Value=$DBOXNAME}]' | head -1 | awk '{ print $6 }'`
+  export VOL_ID=`aws ec2 create-volume --no-encrypted --availability-zone $MY_ZONE --size 30 --volume-type standard --tag-specifications 'ResourceType=volume,Tags=[{Key=Name,Value='$DBOXNAME'}]' | head -1 | awk '{ print $6 }'`
   echo Created $VOL_ID in $MY_ZONE
 else
   echo Volume $VOL_ID already available, using it
