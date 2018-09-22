6ec25aaea9afbffb151a2084dd4cf57cc7c9da42
Andrew Berkley <ajberkley@gmail.com>
2018-09-22 10:41:00 -0700

    Controllable shutdown

--- continental-test/do-rasp-run-continental.sh
+++ continental-test/do-rasp-run-continental.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # ajb June 2018 main driver script for running a continental or west HRDPS visualization run
-# Call this as do-rasp-run-continental.sh
+# Call this as do-rasp-run-continental.sh [ false | true ]
 # If you have already downloaded some data this will not re-download it.
 # You can export NODOWNLOAD=1 to not re-download data, make sure to mount /mnt first
-
+SHUTDOWN=${1:-false}
 ./setup-drives.sh
 PATH=$PATH:/home/ubuntu/canadarasp/aws-utils:/home/ubuntu/.local/bin
 MY_INSTANCE_ID=`get-my-instance-id.sh`
 MODEL=`aws-read-tag.sh $MY_INSTANCE_ID model`
 MODEL=${MODEL:-$1}
 export MODEL=${MODEL:-"gdps"}
-echo Running RASP for model $MODEL
+echo Running RASP for model $MODEL, SHUTDOWN=$SHUTDOWN
 source ./model-parameters.sh $MODEL
 source ./guess-time.sh $MODEL
 echo Moving downloaded data to local disk starting at `date`
@@ -34,6 +34,9 @@ if [ -z $NOPLOT ]; then     # if string is NULL
  ./do-hrdps-plots-continental.sh $YEAR $MONTH $DAY $HOUR > hrdps-plots.log 2>&1
 fi
 
-echo "Finished at `date`, shutting down"
-sleep 30
-#sudo shutdown -h now
+echo "Finished at `date`"
+if [ $SHUTDOWN == "true" ] ; then
+  echo "Shutting down 30 seconds"
+  sleep 30
+  sudo shutdown -h now
+fi
