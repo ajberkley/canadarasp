ed19eed40e98fd1752f613836c86b67eed0b86cb
Andrew Berkley <ajberkley@gmail.com>
2019-03-08 20:19:37 -0800

    Fix archiving old maps... do it after upload

--- continental-test/upload-map-pngs.sh
+++ continental-test/upload-map-pngs.sh
@@ -2,8 +2,6 @@
 source ./model-parameters.sh $MODEL
 WEBSERVERIP=`./webserver-ip.sh`
 BASEDIR=`echo $PNGDIR | sed s/[/]mnt/html/g`
-echo "Archiving old maps for BC"
-ssh -i ~/.ssh/montreal.pem ubuntu@$WEBSERVERIP "(cd canadarasp ; ./archive-old-map-pngs.sh)"
 echo "Deleting old files"
 ssh -i ~/.ssh/montreal.pem ubuntu@$WEBSERVERIP "(cd canadarasp ; ./delete-old-map-pngs.sh)"
 echo "Uploading files to $WEBSERVERIP to directory $BASEDIR"
@@ -19,3 +17,5 @@ echo "Done uploading files, updating latest link"
 ssh -i ~/.ssh/montreal.pem ubuntu@$WEBSERVERIP "(cd $BASEDIR; rm -f latest ; ln -s `ls -1rt | tail -1` latest )"
 echo "Deleting old map-pngs again"
 ssh -i ~/.ssh/montreal.pem ubuntu@$WEBSERVERIP "(cd canadarasp ; ./delete-old-map-pngs.sh)"
+echo "Archiving old maps for BC"
+ssh -i ~/.ssh/montreal.pem ubuntu@$WEBSERVERIP "(cd canadarasp ; ./archive-old-map-pngs.sh)"
