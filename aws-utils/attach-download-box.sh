c20f9cef4cdeaf6ff73e74d80ce8196b1b77ecc6
Andrew Berkley <ajberkley@gmail.com>
2019-03-13 00:51:56 +0000

    Have to wait awhile for disk to be availble

--- aws-utils/attach-download-box.sh
+++ aws-utils/attach-download-box.sh
@@ -10,10 +10,11 @@ export MY_INSTANCE_ID=`curl -s http://169.254.169.254/latest/meta-data/instance-
 export MY_ZONE=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
 aws ec2 wait volume-available --volume-ids $VOL_ID
 echo $VOL_ID available
-aws ec2 attach-volume --device /dev/sdf --instance $MY_INSTANCE_ID --volume-id $VOL_ID
+aws ec2 attach-volume --device /dev/sdh --instance $MY_INSTANCE_ID --volume-id $VOL_ID
 echo Attaching $VOL_ID to $MY_INSTANCE_ID
 aws ec2 wait volume-in-use --volume-ids $VOL_ID
 echo Attached $VOL_ID to $MY_INSTANCE_ID
+sleep 10
 export BLK=`lsblk | grep 30G  | awk '{print $1}'`
 echo BLK is $BLK
 export BLK_DEV="/dev/$BLK"
