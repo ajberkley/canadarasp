e420c384d822b411ff23632480e85a19e2babc54
Andrew Berkley <ajberkley@gmail.com>
2019-03-13 00:44:22 +0000

    Bugfixes

---
+++ aws-utils/attach-download-box.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Create and mount a standard volume on EC2 on $1 unless $1 is not provided
+# First check to make sure a download-box does not exist already
+MNT=${1:-NOMNT}
+export -n VOL_ID=
+export DBOXNAME="download-box-$MODEL"
+echo Going to get a $DBOXNAME
+export VOL_ID=`aws ec2 describe-volumes --filters Name=tag:Name,Values=$DBOXNAME Name=status,Values=available | head -1 | awk '{ print $7 }'`
+export MY_INSTANCE_ID=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
+export MY_ZONE=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
+aws ec2 wait volume-available --volume-ids $VOL_ID
+echo $VOL_ID available
+aws ec2 attach-volume --device /dev/sdf --instance $MY_INSTANCE_ID --volume-id $VOL_ID
+echo Attaching $VOL_ID to $MY_INSTANCE_ID
+aws ec2 wait volume-in-use --volume-ids $VOL_ID
+echo Attached $VOL_ID to $MY_INSTANCE_ID
+export BLK=`lsblk | grep 30G  | awk '{print $1}'`
+echo BLK is $BLK
+export BLK_DEV="/dev/$BLK"
+echo Going to mount $BLK_DEV
--- aws-utils/create-download-box.sh
+++ aws-utils/create-download-box.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Create and mount a standard volume on EC2 on $1
 # First check to make sure a download-box does not exist already
-MNT=${1:-/mnt}
 export -n VOL_ID=
 export DBOXNAME="download-box-$MODEL"
-echo Going to get a $DBOXNAME and mount it at $MNT
+export MNT="/$DBOXNAME"
+echo Going to get a $DBOXNAME and mount it at /$DBOXNAME
 export VOL_ID=`aws ec2 describe-volumes --filters Name=tag:Name,Values=$DBOXNAME Name=status,Values=available | head -1 | awk '{ print $7 }'`
 export MY_INSTANCE_ID=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
 export MY_ZONE=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
@@ -24,7 +24,7 @@ else
 fi
 aws ec2 wait volume-available --volume-ids $VOL_ID
 echo $VOL_ID available
-source ./mount-download-box.sh /$DBOXNAME
+source ./attach-download-box.sh 
 sleep 5
 sudo mkfs.ext4 $BLK_DEV
 sudo mkdir -p $MNT
