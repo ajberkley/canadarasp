911f85044f0dd858bac43af0303e22498f6b75f7
Andrew Berkley <ajberkley@gmail.com>
2019-04-08 15:46:16 +0000

    Make device attachment a little more robust

--- aws-utils/attach-download-box.sh
+++ aws-utils/attach-download-box.sh
@@ -12,8 +12,11 @@ aws ec2 attach-volume --device /dev/sdh --instance $MY_INSTANCE_ID --volume-id $
 echo Attaching $VOL_ID to $MY_INSTANCE_ID
 aws ec2 wait volume-in-use --volume-ids $VOL_ID
 echo Attached $VOL_ID to $MY_INSTANCE_ID
-sleep 10
-export BLK=`lsblk | grep 30G  | awk '{print $1}'`
-echo BLK is $BLK
+export BLK=""
+while [ "$BLK" == "" ]; do
+  sleep 10
+  export BLK=`lsblk | grep 30G  | awk '{print $1}'`
+  echo BLK is $BLK
+done
 export BLK_DEV="/dev/$BLK"
 echo Going to mount $BLK_DEV
