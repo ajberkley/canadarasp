b9b6adbf4bd91054de547b0b12cb1053035cc819
Andrew Berkley <ajberkley@gmail.com>
2019-05-22 21:15:51 +0000

    Change instance names

--- aws-utils/stop-compute-server.sh
+++ aws-utils/stop-compute-server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-INSTANCE_NAME=${1:-"HRDPS DEV 4.1"}
+INSTANCE_NAME=${1:-"HRDPS PROD V6"}
 INSTANCE_ID=`aws ec2 describe-instances --filters Name=tag:Name,Values="$INSTANCE_NAME" | grep INSTANCES | awk '{ print $8 }'`
 if [ -z $INSTANCE_ID ]; then
     echo Could not find $INSTANCE_NAME
