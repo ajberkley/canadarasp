2d91bd5fbf12e57df631f2918ad6cf3c7b40d6a0
Andrew Berkley <ajberkley@gmail.com>
2019-05-23 17:25:50 +0000

    Update more compute names

--- aws-utils/get-compute-server-id.sh
+++ aws-utils/get-compute-server-id.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-INSTANCE_NAME="HRDPS DEV 4.1"
+INSTANCE_NAME="HRDPS PROD V6"
 aws ec2 describe-instances --filters Name=tag:Name,Values="$INSTANCE_NAME" | grep INSTANCES | awk '{ print $8 }'
