14cc6e0dbfeef8017a6c347942e16236c3159504
Andrew Berkley <ajberkley@gmail.com>
2019-08-11 16:15:13 +0000

    Add New England to regions

--- web-server/regions.js
+++ web-server/regions.js
@@ -1,2 +1,2 @@
-var regions = [ "Lower Mainland", "Sea to Sky", "Vancouver Island", "Kamloops Area","North Okanagan","South Okanagan","Kootenays","Smithers Area","Jasper Area","Alberta","Washington","Ski","Cariboo", "Montreal", "Yukon", "Saskatchewan", "Manitoba", "Montana", "Alaska"];
+var regions = [ "Lower Mainland", "Sea to Sky", "Vancouver Island", "Kamloops Area","North Okanagan","South Okanagan","Kootenays","Smithers Area","Jasper Area","Alberta","Washington","Ski","Cariboo", "Montreal", "Yukon", "Saskatchewan", "Manitoba", "Montana", "Alaska", "New England"];
 
