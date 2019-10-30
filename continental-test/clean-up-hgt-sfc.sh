98cdd79339c2d72dc799f450370afca4a8ba8566
Andrew Berkley <ajberkley@gmail.com>
2019-10-30 13:49:20 -0700

    Clean up HGT SFC fix

--- continental-test/clean-up-hgt-sfc.sh
+++ continental-test/clean-up-hgt-sfc.sh
@@ -6,7 +6,7 @@ echo Cleaning up HGT_SFC mess
 HGT_SFC_END=$( filename HGT_SFC_0 $TIMESTOP )
 HGT_SFC_ZERO=$( filename HGT_SFC_0 0$TIMESTART )
 # HRDPS has HGT_SFC for > 0 only, GDPS has HGT_SFC for 0 only
-if [ -f $HGT_SFC_ZERO ]; then # GDPS
+if [ $MODEL == gdps ]; then # GDPS
  echo Fixing GDPS HGT_SFC to exist always
  TIMEONE=$(( $TIMESTART + $TIMESTEP ))
  GOODTIMES=($(seq -w $TIMEONE $TIMESTEP $TIMESTOP))
