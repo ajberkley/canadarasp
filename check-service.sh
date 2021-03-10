e363921222b2261692c3e34e54b9a82822a837db
Andrew Berkley <ajberkley@gmail.com>
2021-03-10 12:54:06 +0000

    Add monitoring to dynamic windgram

--- check-service.sh
+++ check-service.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 # Run at 8PM each evening PT 
-rm hrdpswindgram0.png
-wget http://www.canadarasp.com/windgrams-data/oneDay/tomorrow/hrdpswindgram0.png
+rm -f hrdpswindgram0.png
+wget --quiet http://www.canadarasp.com/windgrams-data/oneDay/tomorrow/hrdpswindgram0.png
 if [ -f hrdpswindgram0.png ]; then
    aws cloudwatch put-metric-data --namespace canadarasp --metric-name hrdps_monitor_tomorrow --value 1
 fi
 rm hrdpswindgram0.png
-wget http://www.canadarasp.com/windgrams-data/oneDay/dayaftertomorrow/hrdpswindgram0.png
+wget --quiet http://www.canadarasp.com/windgrams-data/oneDay/dayaftertomorrow/hrdpswindgram0.png
 if [ -f hrdpswindgram0.png ]; then
    aws cloudwatch put-metric-data --namespace canadarasp --metric-name hrdps_monitor_dayaftertomorrow --value 1
 fi
 rm hrdpswindgram0.png
-rm gdpswindgram0.png
-wget http://www.canadarasp.com/windgrams-data/oneDay/dayaftertomorrow/gdpswindgram0.png
+rm -f gdpswindgram0.png
+wget --quiet http://www.canadarasp.com/windgrams-data/oneDay/dayaftertomorrow/gdpswindgram0.png
 if [ -f gdpswindgram0.png ]; then
    aws cloudwatch put-metric-data --namespace canadarasp --metric-name gdps_monitor_dayaftertomorrow --value 1
 fi
 rm gdpswindgram0.png
+DATE=`date +%Y-%m-%d`
+rm -f windgram-dynamic-test.html
+wget --quiet "http://canadarasp.com/windgram?lat=49.07&lon=-122.40&date=$DATE\&interactive=t" -O windgram-dynamic-test.html
+if [ -f windgram-dynamic-test.html ]; then
+   aws cloudwatch put-metric-data --namespace canadarasp --metric-name dynamic_windgram --value 1
+fi
+rm windgram-dynamic-test.html
