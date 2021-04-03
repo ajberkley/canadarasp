6d3f3afd6ac2530d6606c59705728b9761fe2d65
Andrew Berkley <ajberkley@gmail.com>
2021-04-03 13:48:45 +0000

    fix due to changes to aws tool interfaces

--- aws-utils/create-download-box.sh
+++ aws-utils/create-download-box.sh
@@ -5,7 +5,7 @@ export -n VOL_ID=
 export DBOXNAME="download-box-$MODEL"
 export MNT="/$DBOXNAME"
 echo Going to get a $DBOXNAME and mount it at /$DBOXNAME
-export VOL_ID=`aws ec2 describe-volumes --filters Name=tag:Name,Values=$DBOXNAME Name=status,Values=available | head -1 | awk '{ print $7 }'`
+export VOL_ID=`aws ec2 describe-volumes --filters Name=tag:Name,Values=$DBOXNAME Name=status,Values=available | head -1 | awk '{ print $8 }'`
 export MY_INSTANCE_ID=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
 export MY_ZONE=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
 echo MY_INSTANCE_ID is $MY_INSTANCE_ID
@@ -13,7 +13,7 @@ echo MY_ZONE is $MY_ZONE
 echo VOL_ID is $VOL_ID
 if [ -z $VOL_ID ]; then
   echo No $DBOXNAME available, creating one
-  export VOL_ID=`aws ec2 create-volume --no-encrypted --availability-zone $MY_ZONE --size 30 --volume-type standard --tag-specifications 'ResourceType=volume,Tags=[{Key=Name,Value='$DBOXNAME'}]' | head -1 | awk '{ print $6 }'`
+  export VOL_ID=`aws ec2 create-volume --no-encrypted --availability-zone $MY_ZONE --size 30 --volume-type standard --tag-specifications 'ResourceType=volume,Tags=[{Key=Name,Value='$DBOXNAME'}]' | head -1 | awk '{ print $7 }'`
   if [ -z $VOL_ID ]; then
     echo Failed creating one.  Aborting
     exit 1
