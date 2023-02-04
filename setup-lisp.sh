4f4c305ea4057cfd8c81ed4eaef0575aa9f5b91a
Andrew Berkley <ajberkley@gmail.com>
2023-02-04 18:23:22 +0000

    Change CL-PNG to point to my copy and precompile it

--- setup-lisp.sh
+++ setup-lisp.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 mkdir -p ~/.config/common-lisp/source-registry.conf.d
 cd /home/ubuntu
-git clone https://github.com/ljosa/cl-png.git
+git clone https://github.com/ajberkley/cl-png.git
 git clone https://github.com/ajberkley/cl-gdal.git
 git clone https://github.com/ajberkley/mmap-shared-cache.git
 echo '(:tree "/home/ubuntu/cl-png")' > ~/.config/common-lisp/source-registry.conf.d/10-cl-png.conf
 echo '(:tree "/home/ubuntu/cl-gdal")' > ~/.config/common-lisp/source-registry.conf.d/11-cl-gdal.conf
 echo '(:tree "/home/ubuntu/mmap-shared-cache")' > ~/.config/common-lisp/source-registry.conf.d/12-mmap-shared-cache.conf
-sbcl --eval '(progn (quicklisp:quickload "osicat") (quicklisp:quickload "alexandria") (quicklisp:quickload "cl-gd") (quicklisp:quickload "cl-ppcre") (quit))'
+sbcl --eval '(progn (quicklisp:quickload "png") (quicklisp:quickload "osicat") (quicklisp:quickload "alexandria") (quicklisp:quickload "cl-gd") (quicklisp:quickload "cl-ppcre") (quit))'
