ea318939796532ac8d2386cee5e9b145a325e63e
Andrew Berkley <ajberkley@gmail.com>
2023-02-05 19:53:26 +0000

    More attempts at fixing efs stuff

--- aws-utils/attach-download-box.sh
+++ aws-utils/attach-download-box.sh
@@ -3,9 +3,15 @@
 export -n VOL_ID=
 export DBOXNAME=${1:-"download-box-$MODEL"}
 echo Going to get a $DBOXNAME 
-export VOL_ID=`aws --output json ec2 describe-volumes --filters Name=tag:Name,Values=$DBOXNAME Name=status,Values=available | jq .Volumes[0]."VolumeId" | tr -d '"'`
+export VOL_ID=`aws --output json ec2 describe-volumes --filters Name=tag:Name,Values=$DBOXNAME | jq .Volumes[0]."VolumeId" | tr -d '"'`
 export MY_INSTANCE_ID=`curl -s http://169.254.169.254/latest/meta-data/instance-id`
 export MY_ZONE=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
+if [ $VOL_ID == "null" ]; then
+  echo VOL_ID not correct, trying again...
+  sleep 10 # sometimes things just go badly
+  export VOL_ID=`aws --output json ec2 describe-volumes --filters Name=tag:Name,Values=$DBOXNAME | jq .Volumes[0]."VolumeId" | tr -d '"'`
+fi
+echo Trying to use $VOL_ID
 aws ec2 wait volume-available --volume-ids $VOL_ID
 echo $VOL_ID available
 aws ec2 attach-volume --device /dev/sdh --instance $MY_INSTANCE_ID --volume-id $VOL_ID
